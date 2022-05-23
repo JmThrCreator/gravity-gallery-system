10b7acfe5c5098418c39ede4f63758fe543b12ed
JmThrCreator <jmthrcreator@gmail.com>
2022-05-23 21:35:55 +0100

    gallery responsiveness

--- app/static/js/components/box.js
+++ app/static/js/components/box.js
@@ -1,16 +1,23 @@
 class Box {
     constructor(x, y, w, h, img) {
+
+
+        var scale
+        var widthMultiplier
+        if (width < 800) scale = 0.25, widthMultiplier = 2;
+        else scale = 0.5, widthMultiplier = 1;
+
         let options = {
             //chamfer: { radius:10 },
             render: {
                 sprite: {
                     texture: staticFolder + img,
-                    xScale: 0.5,
-                    yScale: 0.5,
+                    xScale: scale,
+                    yScale: scale,
                 }
             }
         }
-        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.body = Bodies.rectangle(x, y, w/widthMultiplier, h/widthMultiplier, options);
         World.add(world, this.body);
     }
 }
--- app/static/js/components/stack.js
+++ app/static/js/components/stack.js
@@ -2,33 +2,34 @@ class Gallery {
     constructor(x, y, nx, ny, w, h) {
         var length = imageList.length;
 
-        var nx = nx;
-
+        if (width < 800) x = width/2-140, y = height/4, w = 20, h = 20;
 
         let stack = Composites.stack(x, y, nx, ny, w, h, function(x, y, ix, iy) {
 
             if (iy == 1) ix += 3;
             else if (iy == 2) ix += 6;
 
-
             if (imageList.length <= ix) return;
+
+            console.log(width*0.001, imageList[ix].width)
             
+            var scale
+            var widthMultiplier
+            if (width < 800) scale = 0.25, widthMultiplier = 4;
+            else scale = 0.5, widthMultiplier = 2;
+
             let options = {
                 //chamfer: { radius:10 },
                 render: {
                     sprite: {
                         texture: staticFolder + imageList[ix].path,
-                        xScale: 0.5,
-                        yScale: 0.5,
+                        xScale: scale,
+                        yScale: scale,
                     }
                 }
             }
 
-            if (width <= 400) {
-                
-            }
-
-            return Bodies.rectangle(x, y, imageList[ix].width/2, imageList[ix].height/2, options);
+            return Bodies.rectangle(x, y, imageList[ix].width/widthMultiplier, imageList[ix].height/widthMultiplier, options);
         });
 
         World.add(world, stack);
